1772de21e2f2c3db8742ba9fe0a9ef24d5a70d48
Kathiavega <71344309+Kathiavega@users.noreply.github.com>
2020-12-11 20:28:48 -0600

    Delete Persona.cs

--- Ejercicios/Agenda/Persona.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Persona
-{
-    public int Codigo { get; set; }
-    public string Nombre { get; set; }
-    public string Apellido { get; set; }
-    public string Telefono { get; set; }
-}
